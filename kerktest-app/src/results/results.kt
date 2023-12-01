5f0fb184edc0d0009b7e1285ee189ee25c4434cd
BlockCat <zinoonomiwo@gmail.com>
2023-12-01 08:27:14 +0100

    commit stuff

--- kerktest-app/src/results/results.kt
+++ kerktest-app/src/results/results.kt
@@ -9,49 +9,52 @@ import kotlin.math.roundToInt
 
 @JsName("gtag")
 external fun gtag(method: String, tag: String, payload: Json)
-
+// Displaying this part means that the results are in.
+// Churches is the list with all kind of different churches
+// Answers is the mapping to which answer is given to what question.
+// questionImportance is the setting how important every question is.
 fun RBuilder.results(churches: List<Church>, answers: Map<Question, Answer?>, questionImportance: Map<Question, Double>) {
 
 
-    div("signpost"){
-        div("results") {
-            table("table") {
-                thead {
-                    tr {
-                        th { +"Score" }
-                        th { +"Stroming" }
-                    }
-                }
-                tbody {
-                    answers.toList().forEachIndexed { index, (question, answer) ->
-                        val questionIndex = index
-                        val importance = answer?.important
-                        val answerId = question.answers.indexOf(answer)
-                        val payload = json(
-                                Pair("Question", "$questionIndex"),
-                                Pair("Answer", "$answerId"),
-                                Pair("Importance", "$importance"))
-
-                        gtag("config", "UA-121801911-1", json(
-                                Pair("custom_map", json(
-                                        Pair("dimension2", "Question"),
-                                        Pair("metric3", "Answer"),
-                                        Pair("metric4", "Importance")
-                                ))
-                        ))
-                        gtag("event", "answers", payload)
-                    }
-
-                    // Calculate the maximum amount of points a church can have
-                    val completeSum = answers.keys.filter { !it.ignore }.sumByDouble {
-                        (questionImportance[it] ?: 1.0) * (answers[it]?.important ?: 1)
-                    }
-
-                    calculateStuff(churches, answers, questionImportance).forEach { (church, value) ->
-                        entry(church, value / completeSum)
-                    }
+    div("results") {
+        table("table") {
+            thead {
+                tr {
+                    th { +"Score" }
+                    th { +"Stroming" }
                 }
             }
+            tbody {
+                answers.toList().forEachIndexed { index, (question, answer) ->
+                    val questionIndex = index
+                    val importance = answer?.important
+                    val answerId = question.answers.indexOf(answer)
+                    val payload = json(
+                            Pair("Question", "$questionIndex"),
+                            Pair("Answer", "$answerId"),
+                            Pair("Importance", "$importance"))
+
+                    gtag("config", "UA-121801911-1", json(
+                            Pair("custom_map", json(
+                                    Pair("dimension2", "Question"),
+                                    Pair("metric3", "Answer"),
+                                    Pair("metric4", "Importance")
+                            ))
+                    ))
+                    gtag("event", "answers", payload)
+                }
+
+                // Calculate the maximum amount of points a church can have
+                val completeSum = answers.keys.filter { !it.ignore }.sumByDouble {
+                    (questionImportance[it] ?: 1.0) * (answers[it]?.important ?: 1)
+                }
+
+                calculateStuff(churches, answers, questionImportance).forEach { (church, value) ->
+                    entry(church, value / completeSum)
+                }
+            }
+        }
+        div("signpost") {
         }
     }
 }
